8058b54afd07a378e3215222823aebe1042252a2
Uliana-Murzik <keti-03@mail.ru>
2024-10-23 15:48:17 +0300

    addedpages

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './ui/NavBar';
 
 export default function Layout() {
   return (
     <div>
-      
+      <NavBar />
+      <Outlet />
     </div>
-  )
+  );
 }
-
