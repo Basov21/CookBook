6bbb4d07444f1ea429cac640b31bea69b8f539df
Basov21 <id.basov@yandex.ru>
2024-10-23 19:01:01 +0300

    db created

---
+++ server/db/models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Recipe }) {
+      this.belongsToMany(Recipe, { through: 'Favourites', foreignKey: 'userId' });
+    }
+  }
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      hashpass: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
+  return User;
+};
