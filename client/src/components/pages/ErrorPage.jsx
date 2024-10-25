6fbbfa9db8ac870dc327d111f54b8dfcce5fccbd
Uliana-Murzik <keti-03@mail.ru>
2024-10-25 11:12:12 +0300

    added error page

--- client/src/components/pages/ErrorPage.jsx
+++ client/src/components/pages/ErrorPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Container, Image } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 5000)
+    return () => clearTimeout(timer);
+  },[navigate])
+
+
   return (
-    <div>ErrorPage</div>
+    
+    <Container  className="d-flex justify-content-center align-items-center" >
+<div >
+          <Image src="../../../public/img/photo_5249117529920626948_y.jpg" style={{height: "750px"}}/>
+          
+          </div>
+
+       
+
+    </Container>
+  
   )
 }
