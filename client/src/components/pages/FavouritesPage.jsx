abc0e3e56110799449d559645f018b2d24e9c76e
EvgenyShchelchkov <galaxyaseria007@mail.ru>
2024-10-25 16:56:24 +0300

    wwwwww

--- client/src/components/pages/FavouritesPage.jsx
+++ client/src/components/pages/FavouritesPage.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import axiosInstance from "../../api/axiosInstance";
-import RecipeCard from "../ui/RecipeCard";
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../api/axiosInstance';
+import RecipeCard from '../ui/RecipeCard';
 
-
-export default function FavouritesPage({ favouriteRecipes, handleAddFavourite }) {
+export default function FavouritesPage({ favouriteRecipes, handleAddFavourite}) {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
@@ -25,17 +24,24 @@ export default function FavouritesPage({ favouriteRecipes, handleAddFavourite })
   }
 
   if (recipes.length === 0) {
-    return <div style= {{textAlign: 'center', marginTop: '50px', fontSize: '20px', color: 'red'}}>Вы не добавили ни одного рецепта</div>;
+    return (
+      <div
+        style={{ textAlign: 'center', marginTop: '50px', fontSize: '20px', color: 'red' }}
+      >
+        Вы не добавили ни одного рецепта
+      </div>
+    );
   }
 
-return (
-
-  <div>
-    {console.log(favouriteRecipes)}
-    {recipes.map((recipe) => (
-      <RecipeCard key={recipe.id} recipe={recipe} handleAddFavourite={handleAddFavourite} onDelete={()=>handleDeleteFav(recipe.id)}/>))}
-  </div>
-
-)
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          recipe={recipe}
+          handleAddFavourite={handleAddFavourite}
+        />
+      ))}
+    </div>
+  );
 }
-
--- server/src/routes/recipeRouter.js
+++ server/src/routes/recipeRouter.js
@@ -1,34 +1,29 @@
 const { Router } = require('express');
 const { Recipe } = require('../../db/models');
+const {
+  RelationshipType,
+} = require('sequelize/lib/errors/database/foreign-key-constraint-error');
 const recipeRouter = Router();
 
-recipeRouter
-  .route('/')
-  .get(async (req, res) => {
-    try {
-      const recipes = await Recipe.findAll(); // ?
-      res.json(recipes);
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ text: 'Ошибка получения карточек', message: error.message });
-    }
-  })
+recipeRouter.route('/').get(async (req, res) => {
+  try {
+    const recipes = await Recipe.findAll(); // ?
+    res.json(recipes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ text: 'Ошибка получения карточек', message: error.message });
+  }
+});
 
-  recipeRouter
-  .route('/:id')
-  .get(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const recipes = await Recipe.findOne({where: {id}}); 
-      res.json(recipes);
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ text: 'Ошибка получения карточки', message: error.message });
-    }
-  })
+recipeRouter.route('/:id').get(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const recipes = await Recipe.findOne({ where: { id } });
+    res.json(recipes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ text: 'Ошибка получения карточки', message: error.message });
+  }
+});
 
 module.exports = recipeRouter;
