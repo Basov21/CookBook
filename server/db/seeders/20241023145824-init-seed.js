8a7f6fa84c6ef0cf8a02561d28be76ecf04b2ed2
natashik7 <nata_kryl@list.ru>
2024-10-25 12:23:53 +0300

    done favourite

--- server/db/seeders/20241023145824-init-seed.js
+++ server/db/seeders/20241023145824-init-seed.js
@@ -165,6 +165,10 @@ module.exports = {
         userId: 1,
         recipeId: 2,
       },
+      {
+        userId: 1,
+        recipeId: 4,
+      }
     ]);
   },
 
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,41 +1,36 @@
 const express = require('express');
 const { Favourite } = require('../../db/models');
+const { Recipe } = require('../../db/models');
+const { User } = require('../../db/models');
 const favouriteRouter = express.Router();
-const verifyAccessToken = require('../middlewares/verifyAccessToken')
+const verifyAccessToken = require('../middlewares/verifyAccessToken');
 
 favouriteRouter
   .route('/')
   .get(verifyAccessToken, async (req, res) => {
-    const userId = res.locals.user.id;
-    const favourites = await Favourite.findAll({ where: { userId} });
-    res.json(favourites);
-    console.log(favourites);
-    
-    // const favourites = await Favourite.findAll({
-    //   where: { userId }})
-    
-    //   res.json(favourites)
-    //   include: {
-    //     model: Recipe,
-    //   },
-    // })
-    // const recipes = favourites.map(fav => fav.recipe);
-    // res.json(recipes)
-    // console.log(recipes);
-    
-  })
+    try {
+      const userId = res.locals.user.id;
+    const recipe = await User.findByPk(userId, {
+      include: {
+        model: Recipe,
+        as: 'favourites',
+      }
+    },)
+    res.status(200).json(recipe.favourites)
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'No recipes founf' });
+    }
+     })
 
   .post(verifyAccessToken, async (req, res) => {
-    // const { id } = req.params;
-    // const { recipeId } = req.body;
-    // const favourite = await Favourite.create({  userId: res.locals.user.id, recipeId:req.body.recipeId});
-    // res.json(favourite);
     try {
       const { recipeId } = req.body;
       const userId = res.locals.user.id;
 
       const favourite = await Favourite.create({ userId, recipeId });
-      res.json(favourite);
+      res.status(200).json(favourite);
+      
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Failed to add favourite' });
